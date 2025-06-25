a1ee72bb05b1bb36ca05f3e0b341eec45dcd6b25
stv230v <stv230v@gmail.com>
2025-06-25 21:41:55 +0900

    チャットアプリの作成

---
+++ frontend/src/app/api/response/route.ts
@@ -0,0 +1,14 @@
+// app/api/response/route.ts
+
+import { NextResponse } from "next/server";
+import { openaiClient, sendPromptToGpt } from "@/app/services/openai-service";
+
+export async function POST(request: Request) {
+  const openai = openaiClient();
+
+  const { prompt } = await request.json();
+
+  const gptResponseMessage = await sendPromptToGpt(prompt);
+  const response = NextResponse.json({ gptResponseMessage });
+  return response;
+}
---
+++ frontend/src/components/ChatMessage.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import { useRecoilState } from "recoil";
+import { chatLogState } from "../states/ChatLogState";
+
+type MessageType = {
+  id: number;
+  content: string;
+  sender: string;
+};
+
+const ChatMessage = () => {
+  const [chatLog, setChatLog] = useRecoilState(chatLogState);
+
+  return (
+    <>
+      {chatLog.map((message: MessageType) => {
+        return (
+          <div
+            key={message.id}
+            className={`flex items-end ${
+              message.sender === "user" ? "justify-end" : ""
+            }`}
+          >
+            {message.sender === "other" && (
+              <div className="flex-shrink-0 mr-2">
+                <div className="h-8 w-8 bg-gray-300 rounded-full" />
+              </div>
+            )}
+            <div
+              className={`rounded p-2 ${
+                message.sender === "user" ? "bg-blue-500" : "bg-gray-500"
+              }`}
+            >
+              <p className="text-sm">{message.content}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ChatMessage;
